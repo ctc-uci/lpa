98d881e9bb8fc3b970419d1099355f4762c7337b
Nathan Pietrantonio <78774649+theNatePi@users.noreply.github.com>
2024-12-03 21:09:25 -0800

    Replace clients with PostgreSQL

--- server/db/schema/clients.sql
+++ server/db/schema/clients.sql
@@ -1,9 +1,7 @@
-CREATE TYPE client_role AS ENUM ('payee', 'instructor');
-
-CREATE TABLE clients (
-  id INT PRIMARY KEY UNIQUE NOT NULL DEFAULT nextval('client_id_seq'),
-  event_id INT REFERENCES events(id) NOT NULL,
-  role client_role NOT NULL,
-  name VARCHAR(256) NOT NULL,
-  email VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS public.clients
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT client_pkey PRIMARY KEY (id)
+)
