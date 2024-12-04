57e48ec98ce2b5ede7463031560f390ae33f45a8
Nathan Pietrantonio <78774649+theNatePi@users.noreply.github.com>
2024-12-03 21:10:19 -0800

    Replace events with PostgreSQL

--- server/db/schema/events.sql
+++ server/db/schema/events.sql
@@ -1,6 +1,8 @@
-CREATE TABLE events (
-  id INT PRIMARY KEY UNIQUE NOT NULL DEFAULT nextval('event_id_seq'),
-  name VARCHAR(256) NOT NULL,
-  description VARCHAR(256),
-  archived BOOL NOT NULL DEFAULT False
+CREATE TABLE IF NOT EXISTS public.events
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    description character varying(256) COLLATE pg_catalog."default",
+    archived boolean NOT NULL DEFAULT false,
+    CONSTRAINT event_pkey PRIMARY KEY (id)
 );
