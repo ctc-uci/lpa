57e773aedbd805005c6a64fbe84ffce4969f57cf
Priska Kunaidy <kunaidyp@uci.edu>
2025-01-03 16:44:43 -0800

    finished writing requests

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,6 +8,29 @@ const usersRouter = express.Router();
 usersRouter.use(express.json());
 
 // Get a user by email
+usersRouter.get("/email/:email", async (req, res) => {
+  try {
+    const { email } = req.params;
+
+    const user = await db.query(
+      `
+        SELECT *
+        FROM users
+        WHERE email = $1
+      `,
+      [email,]);
+
+    // User with email not found
+    if (!(user.length)) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(keysToCamel(user[0]));
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 
 // Get a user by ID
@@ -21,13 +44,31 @@ usersRouter.get("/:id", async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json(keysToCamel(user[0]));
+    res.status(200).json(keysToCamel(user));
 
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
+
+// Create new user, returns new user ID
+usersRouter.post("", async (req, res) => {
+  try {
+    const { email, firebaseUid, firstName, lastName, editPerms } = req.body;
+    const user = await db.query (
+      `
+        INSERT INTO users (email, firebase_uid, first_name, last_name, edit_perms)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id;
+      `,
+      [email, firebaseUid, firstName, lastName, editPerms]);
+    res.status(200).json(keysToCamel(user[0]));
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 // Update a user by ID
 usersRouter.put('/:id', async (req, res) => {
