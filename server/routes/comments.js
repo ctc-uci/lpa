afb8254760b45909adf0eda1075eb0c992f145f3
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2025-01-14 14:16:17 -0800

    comments router remove comments and add spacing

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -10,30 +10,29 @@ commentsRouter.get("/:id", async (req, res) => {
     try {
         const {id} = req.params;
         const data = await db.query("SELECT * FROM comments WHERE id = $1", [id]);
+
         res.status(200).json(keysToCamel(data));
     } catch (err) {
         res.status(500).send(err.message);
     }
 });
 
-// GET        /comments/invoice/:id
-// Returns all comments that much up that invoice ID
 commentsRouter.get("/invoice/:id", async (req, res) => {
     try {
         const {id} = req.params;
         const data = await db.query("SELECT * FROM comments WHERE invoice_id = $1", [id]);
+
         res.status(200).json(keysToCamel(data));
     } catch (err) {
         res.status(500).send(err.message);
     }
 });
 
-// GET        /comments/booking/:id
-// Returns all comments that much up that invoice ID
 commentsRouter.get("/booking/:id", async (req, res) => {
     try {
         const {id} = req.params;
         const data = await db.query("SELECT * FROM comments WHERE booking_id = $1", [id]);
+        
         res.status(200).json(keysToCamel(data));
     } catch (err) {
         res.status(500).send(err.message);
