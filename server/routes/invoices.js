6017e05a31d04ababed8c705bcc84d53a7a228e9
Kaitlin Leung <leungkn@uci.edu>
2025-01-17 13:32:22 -0800

    finished endpoints?

--- server/routes/invoices.js
+++ server/routes/invoices.js
@@ -34,22 +34,33 @@ invoicesRouter.get("/:id", async (req, res) => {
     }
   });
 
-  // Get invoice for an event by the event id optionally between a start and end date
+// Get invoice for an event by the event id optionally between a start and end date
 invoicesRouter.get("/event/:event_id", async (req, res) => {
   try {
-    const { id } = req.params;
+    const { event_id } = req.params;
     const { start, end } = req.query;
 
-    const invoice = await db.query("SELECT * FROM invoices WHERE id = $1", [
-      id,
-    ]);
+    let query = `SELECT * FROM invoices WHERE event_id = $1`;
+    const params = [event_id];
 
-    if(event.length === 0){
-        return res.status(404).json({ error: "Event does not exist." });
+    if (start) {
+      query += ` AND (start_date >= $2)`;
+      params.push(start);
     }
 
-    res.status(200).json(keysToCamel(event));
-  } catch (err) {
+    if (end) {
+      if (params.length === 1) {
+        query += ` AND (end_date <= $2)`;
+      } else {
+        query += ` AND (end_date <= $3)`;
+      }
+      params.push(end);
+    }
+
+    const data = await db.query(query, params);
+
+    res.status(200).json(keysToCamel(data));
+} catch (err) {
     res.status(500).send(err.message);
   }
 });
