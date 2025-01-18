6e03ecb6547cb4934747dfabef1f0b9f19e4eeec
Cole Thompson <colet0227@gmail.com>
2025-01-17 18:20:30 -0800

    feat: create delete endpoint

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -4,13 +4,31 @@ import { db } from "../db/db-pgp"; // TODO: replace this db with
 
 export const clientsRouter = Router();
 
-// Get all comments
+// Get all clients
 clientsRouter.get("/", async (req, res) => {
   try {
     const users = await db.query(`SELECT * FROM clients ORDER BY id ASC`);
 
     res.status(200).json(keysToCamel(users));
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(500).send(err.message);
   }
 });
+
+// Delete client by id
+clientsRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedClient = await db.query('DELETE FROM clients WHERE id = $1 RETURNING *', [id]);
+
+    if (deletedClient.length === 0) {
+      return res.status(404).json(keysToCamel({result: "error", message: "Client not found"}));
+    }
+
+    res.status(200).json(keysToCamel({result: "success", deletedClient: deletedClient[0]}));
+    
+  } catch (err) {
+    res.status(500).json(keysToCamel({result: "error", message: err.message}));
+  }
+});
+
