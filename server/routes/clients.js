f461a17126ef09c4d9fb7041fe4cb050bd76d861
Yihong Yu <hazelyumyk@gmail.com>
2025-01-17 22:09:02 -0800

    feat: create GET and PUT for client

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -15,6 +15,49 @@ clientsRouter.get("/", async (req, res) => {
   }
 });
 
+// Get client by id
+clientsRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await db.query(`SELECT * FROM clients WHERE id = $1`, [id]);
+
+    // Case when client not found
+    if (user.length === 0) {
+      return res.status(404).json(keysToCamel("Client not found"));
+    }
+
+    res.status(200).json(keysToCamel(user[0]));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Put client by id
+clientsRouter.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const updatedClient = await db.query(
+      `
+      UPDATE clients 
+      SET 
+        name = $1, 
+        email = $2 
+      WHERE id = $3 
+      RETURNING *
+      `, 
+      [name, email, id]);
+
+    if (updatedClient.length === 0) {
+      return res.status(404).json(keysToCamel("Client not found"));
+    }
+
+    res.status(200).json(keysToCamel(updatedClient));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 // Delete client by id
 clientsRouter.delete("/:id", async (req, res) => {
   try {
@@ -28,7 +71,7 @@ clientsRouter.delete("/:id", async (req, res) => {
     res.status(200).json(keysToCamel({result: "success", deletedClient: deletedClient[0]}));
     
   } catch (err) {
-    res.status(500).json(keysToCamel({result: "error", message: err.message}));
+    res.status(500).send(err.message);
   }
 });
 
