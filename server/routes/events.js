bfbe1da1acd79903dd916cecffea5cc3a5b9f3f7
Nathan Pietrantonio <pietrann@uci.edu>
2025-01-26 23:42:05 -0800

    Clean events router

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-
 import { keysToCamel } from "../common/utils";
 import { db } from "../db/db-pgp";
 
@@ -11,7 +10,7 @@ eventsRouter.get("/", async (req, res) => {
     const events = await db.any(`SELECT * FROM events ORDER BY id ASC`);
 
     if(events.length === 0){
-        return res.status(404).json({ error: "No events found." });
+      return res.status(404).json({ error: "No events found." });
     }
 
     res.status(200).json(keysToCamel(events));
@@ -30,7 +29,7 @@ eventsRouter.get("/:id", async (req, res) => {
     ]);
 
     if(event.length === 0){
-        return res.status(404).json({ error: "Event does not exist." });
+      return res.status(404).json({ error: "Event does not exist." });
     }
 
     res.status(200).json(keysToCamel(event));
@@ -45,13 +44,13 @@ eventsRouter.post("/", async (req, res) => {
     const eventData = req.body;
 
     if (!eventData){
-        return res.status(404).json({ error: "Event data is required" });
+      return res.status(404).json({ error: "Event data is required" });
     }
 
     const result = await db.query(
-        "INSERT INTO events (name, description, archived) VALUES ($1, $2, $3) RETURNING id",
-        [eventData.name, eventData.description || null, eventData.archived ?? false]
-      );
+      "INSERT INTO events (name, description, archived) VALUES ($1, $2, $3) RETURNING id",
+      [eventData.name, eventData.description || null, eventData.archived ?? false]
+    );
     res.status(201).json({ id: result[0].id });
   } catch (err) {
     res.status(500).send(err.message);
@@ -65,17 +64,17 @@ eventsRouter.put("/:id", async (req, res) => {
     const { id } = req.params;
 
     if (!eventData){
-        return res.status(404).json({ error: "Event data is required" });
+      return res.status(404).json({ error: "Event data is required" });
     }
 
     const event = await db.query(
       `UPDATE events
-       SET
-           name = COALESCE($1, name),
-           description = COALESCE($2, description),
-           archived = COALESCE($3, archived)
-       WHERE id = $4
-       RETURNING *`,
+      SET
+      name = COALESCE($1, name),
+      description = COALESCE($2, description),
+      archived = COALESCE($3, archived)
+      WHERE id = $4
+      RETURNING *`,
       [eventData.name, eventData.description, eventData.archived, id]
     );
 
@@ -99,6 +98,5 @@ eventsRouter.delete("/:id", async (req, res) => {
     res.status(500).send(err.message);
   }
 });
-
 
 export { eventsRouter };
