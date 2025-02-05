b666f5086bb0ccb4dcb087119d3daa5227a2045c
Aston <astonkichan@gmail.com>
2025-02-05 11:06:12 -0800

    Added some responsiveness to components. Linked amount due to stats component and comments to table.

--- client/src/components/invoices/Invoice.jsx
+++ client/src/components/invoices/Invoice.jsx
@@ -1,117 +1,169 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleLeft } from "react-icons/fa6";
+import { FiEdit, FiExternalLink } from "react-icons/fi";
 import {
-  Heading,
-  VStack,
-  Text,
-  Flex,
-  Button,
-  HStack,
-  IconButton,
-} from "@chakra-ui/react";
-import { InvoicesTable, InvoicesStats } from "./InvoiceComponents";
-import React, { useState, useEffect } from 'react';
-
-import Navbar from "../Navbar";
-
-import {
-  useNavigate,
-  useParams,
-
+    useNavigate,
+    useParams
 } from "react-router-dom";
 
+import {
+    Heading,
+    Text,
+    Flex,
+    Button,
+    IconButton,
+} from "@chakra-ui/react";
+
+import { InvoicePayments, InvoiceStats } from "./InvoiceComponents";
+import Navbar from "../Navbar";
 import { useBackendContext } from "../../contexts/hooks/useBackendContext";
-
-import { FiEdit } from "react-icons/fi";
-import { FaAngleLeft } from "react-icons/fa6";
-
 
 
 export const Invoice = () => {
-  const { id }= useParams()
-  const { backend } = useBackendContext();
-  const navigate = useNavigate();
-  const payeeName = "Aya De Leon"
-  const payeeEmail = "payee@gmail.com"
+    const { id } = useParams()
+    const { backend } = useBackendContext();
+    const navigate = useNavigate();
 
-  const [ comments, setComments ] = useState([]);
+    const payeeName = "Aya De Leon"
+    const payeeEmail = "payee@gmail.com"
 
-  const [ total, setTotoal ] = useState([]);
+    const [total, setTotal] = useState([]);
+    const [invoices, setInvoices] = useState([]);
+    const [comments, setComments] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const commentsResponse = await backend.get('/comments/paidInvoices' + id);
-        // TODO
-        console.log("COMMENTS: ", commentsResponse.data)
-        setComments(commentsResponse.data);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const invoiceTotalResponse = await backend.get("/invoices/total/" + id);
+                console.log("Invoice Total: ", invoiceTotalResponse.data);
+                setTotal(invoiceTotalResponse.data)
 
-        const totalResponse = await backend.get('/invoices/total' + id);
-        console.log("TOTAL: ", commentsResponse)
-        setTotoal(commentsResponse.total);
+                const invoicesResponse = await backend.get("/invoices")
+                console.log("Invoices", invoicesResponse.data);
+                setInvoices(invoicesResponse.data);
 
+                const commentsResponse = await backend.get('/comments/invoice/' + id);
+                console.log("Comments: ", commentsResponse.data);
+                setComments(commentsResponse.data);
+            } catch (error) {
+                console.error("Error fetching comments:", error);
+            }
+        };
+        fetchData();
+    }, [backend, id]);
 
-      } catch (error) {
-        console.error("Error getting comments:", error);
-      }
-    };
-    fetchData();
-  }, [backend]);
+    return (
+        <Flex direction="row" height="100vh" width="100vw">
+            <Navbar />
+            <Flex direction="column" height="100%" width="100%" padding="2.5vw" gap="1.25vw">
+                <Flex direction="row" width="100%">
+                    <IconButton
+                        icon={<FaAngleLeft />}
+                        onClick={() => {
+                            navigate("/invoices");
+                        }}
+                        variant="link"
+                        color="#474849"
+                        fontSize="2.5rem"
+                    >
+                    </IconButton>
+                </Flex>
+                <Flex direction="column" height="100%" width="100%" paddingLeft="2.5vw" paddingRight="2.5vw" gap="1.25vw">
+                    <Flex direction="row" width="100%">
+                        <Heading fontSize="clamp(2rem, 3vw, 3vw)" color="#4E4AE7">
+                            Invoice Details
+                        </Heading>
+                        <Flex direction="row" marginLeft="auto" gap="1rem">
+                            <Button height="100%" borderRadius="30px" backgroundColor="#4E4AE7" color="#FFFFFF" fontSize="clamp(1rem, 2vw, 2vw)" gap="0.5rem">
+                                <FiEdit></FiEdit>
+                                Edit
+                            </Button>
+                            <Button height="100%" borderRadius="30px" backgroundColor="#4E4AE7" color="#FFFFFF" fontSize="clamp(1rem, 2vw, 2vw)" gap="0.5rem">
+                                <FiExternalLink></FiExternalLink>
+                                Preview
+                            </Button>
+                        </Flex>
+                    </Flex>
+                    <Flex direction="row" width="100%" alignItems="center">
+                        <Text fontSize="2.5vw" color="#474849" fontWeight="bold" marginRight="0.5rem">
+                            Program:
+                        </Text>
+                        <Text fontSize="2.5vw" color="#474849">
+                            Immigrant Rights Solidarity Week: Become an Immigration Rights Ambassador Workshop
+                        </Text>
+                    </Flex>
+                    <InvoiceStats
+                        name={payeeName}
+                        email={payeeEmail}
+                        amountDue={total.total}
+                        height="auto"
+                        width="100%"
+                        padding="1.25vw"
+                        headerSize="2.5vw"
+                        textSize="clamp(1rem, 2vw, 2vw)"
+                        color="#474849"
+                    >
+                    </InvoiceStats>
+                    <InvoicePayments
+                        comments={comments}
+                        height="auto"
+                        width="100%"
+                        headerSize="2.5vw"
+                        textSize="clamp(1rem, 1vw, 1vw)"
+                        gap="0.625vw"
+                    >
+                    </InvoicePayments>
+                    {/* <InvoicePayments
+                comments = { comments }
+            >
+            </InvoicePayments> */}
+                </Flex>
+            </Flex>
+        </Flex>
+        // <Navbar>
+        //     <VStack
+        //         spacing={8}
+        //         sx={{ width: 950, marginX: "auto" }}
+        //     >
+        //     {/* back button */}
+        //     <Flex w="115%" justifyContent="flex-start" mt={50}>
+        //         <IconButton
+        //         icon={<FaAngleLeft />}
+        //         onClick={handleBackClick}
+        //         variant="link"
+        //         color="#474849"
+        //         fontSize="30px"
+        //         />
+        //     </Flex>
 
+        //     {/* title and buttons */}
+        //     <Flex w="100%" justifyContent="space-between" alignItems="center" pt={5}>
+        //     <Heading color="#4E4AE7">Invoice Details</Heading>
+        //     <Flex gap={7}>
+        //         <Button backgroundColor='#4E4AE7' color='#FFF' borderRadius={30} gap={1}>
+        //         <FiEdit/> Edit
+        //         </Button>
+        //         <Button backgroundColor='#4E4AE7' color='#FFF' borderRadius={30}>Preview</Button>
+        //     </Flex>
+        //     </Flex>
 
+        //     {/* description */}
+        //     <Flex w="100%" alignItems="center">
+        //         <Text fontWeight="bold" fontSize="22px" color="#474849" mr={2}>Program:</Text>
+        //         <Text fontSize="20px" color="#474849">
+        //         Immigrant Rights Solidarity Week: Become an Immigration Rights Ambassador Workshop
+        //         </Text>
+        //     </Flex>
 
+        //     {/* invoice stats */}
+        //     <InvoicesStats name={payeeName} email={payeeEmail}/>
 
-  const handleBackClick = () => {
-    navigate("/invoices");
-  };
-
-  return (
-
-    <Navbar>
-    <VStack
-      spacing={8}
-      sx={{ width: 950, marginX: "auto" }}
-
-    >
-
-      {/* back button */}
-      <Flex w="115%" justifyContent="flex-start" mt={50}>
-        <IconButton
-          icon={<FaAngleLeft />}
-          onClick={handleBackClick}
-          variant="link"
-          color="#474849"
-          fontSize="30px"
-        />
-      </Flex>
-
-      {/* title and buttons */}
-      <Flex w="100%" justifyContent="space-between" alignItems="center" pt={5}>
-      <Heading color="#4E4AE7">Invoice Details</Heading>
-      <Flex gap={7}>
-        <Button backgroundColor='#4E4AE7' color='#FFF' borderRadius={30} gap={1}>
-          <FiEdit/> Edit
-        </Button>
-        <Button backgroundColor='#4E4AE7' color='#FFF' borderRadius={30}>Preview</Button>
-      </Flex>
-      </Flex>
-
-      {/* description */}
-      <Flex w="100%" alignItems="center">
-        <Text fontWeight="bold" fontSize="22px" color="#474849" mr={2}>Program:</Text>
-        <Text fontSize="20px" color="#474849">
-          Immigrant Rights Solidarity Week: Become an Immigration Rights Ambassador Workshop
-        </Text>
-      </Flex>
-
-      {/* invoice stats */}
-      <InvoicesStats name={payeeName} email={payeeEmail}/>
-
-      {/* invoice table */}
-      <Flex w="100%" alignItems="center">
-        <Text fontWeight="bold" fontSize="22px" color="#474849">Comments</Text>
-      </Flex>
-      <InvoicesTable comments={comments} />
-
-    </VStack>
-    </Navbar>
-  );
+        //     {/* invoice table */}
+        //     <Flex w="100%" alignItems="center">
+        //         <Text fontWeight="bold" fontSize="22px" color="#474849">Comments</Text>
+        //     </Flex>
+        //     <InvoicesTable comments={comments} />
+        //     </VStack>
+        // </Navbar>
+    );
 }
--- client/src/components/invoices/InvoiceComponents.jsx
+++ client/src/components/invoices/InvoiceComponents.jsx
@@ -1,34 +1,29 @@
+import { format } from 'date-fns';
+import React, { useState } from 'react';
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+
 import {
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Button,
-  Flex,
-  Card,
-  Box,
-  Text,
-  Select
+    Button,
+    Flex,
+    Card,
+    Box,
+    Text,
+    Select,
+    TableContainer,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td
 } from '@chakra-ui/react';
 
 
-import React, { useState, useEffect } from 'react';
-import { useBackendContext } from "../../contexts/hooks/useBackendContext";
-import { format } from 'date-fns';
-
-
-export const InvoicesStats = ({ name, email }) => {
-  return (
-    <Flex gap={7} align="center" w="100%">
-
-      {/* Billed To Section */}
-      <Card
+export const InvoiceStats = ({ name, email, amountDue, padding, height, width, headerSize, textSize, color }) => {
+    return (
+        <Flex direction="row" height={height} width={width} gap="2rem">
+            {/* Billed To Section */}
+            {/* <Card
         flex={1}
         h="7em"
         borderRadius={15}
@@ -42,13 +37,25 @@ export const InvoicesStats = ({ name, email }) => {
         <Text fontWeight="bold" fontSize="22px" color="#474849">Billed to:</Text>
         <Text fontSize="20px" color="#474849">{name}</Text>
         <Text fontSize="20px" color="#474849">{email}</Text>
-      </Card>
+      </Card> */}
+            <Card
+                width="25%"
+                padding={padding}
+                borderRadius="15px"
+                borderWidth="1px"
+                color="#D2D2D2"
+            >
+                <Text fontSize={headerSize} fontWeight="bold" color={color}> Billed to: </Text>
+                <Text fontSize={textSize} color={color}> {name} </Text>
+                <Text fontSize={textSize} color={color}> {email} </Text>
+            </Card>
 
-      {/* Invoice Details Section */}
-      <Card
+
+            {/* Invoice Details Section */}
+            {/* <Card
         flex={3}
         h="7em"
-        borderRadius={15}
+        borderRadius={15} 
         borderWidth=".07em"
         color="#D2D2D2"
         boxShadow="none"
@@ -75,108 +82,222 @@ export const InvoicesStats = ({ name, email }) => {
             <Text color="#474849" fontSize="20px" >$20.00</Text>
           </Box>
         </Flex>
-      </Card>
-    </Flex>
+      </Card> */}
+            <Card
+                width="75%"
+                padding={padding}
 
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
 
-
-
-  );
-
+                borderRadius="15px"
+                borderWidth="1px"
+                color="#D2D2D2"
+            >
+                <Flex width="100%" justifyContent="space-between">
+                    <Box>
+                        <Text fontWeight="bold" fontSize={headerSize} color={color}> Billing Period </Text>
+                        <Text color={color} fontSize={textSize}> Jan 1 - Jan 31 </Text>
+                    </Box>
+                    <Box>
+                        <Text fontWeight="bold" fontSize={headerSize} color={color}> Amount Due </Text>
+                        <Text color={color} fontSize={textSize}> ${amountDue} </Text>
+                    </Box>
+                    <Box>
+                        <Text fontWeight="bold" fontSize={headerSize} color={color}> Remaining Balance </Text>
+                        <Text color={color} fontSize={textSize}> $20.00 </Text>
+                    </Box>
+                </Flex>
+            </Card>
+        </Flex>
+    );
 };
 
-export const InvoicesTable = ({ comments }) => {
-  const [pageSize, setPageSize] = useState(3);
-  const [currentPage, setCurrentPage] = useState(1);
+export const InvoicePayments = ({ comments, height, width, headerSize, textSize, gap }) => {
+    const [commentsPerPage, setCommentsPerPage] = useState(3);
+    const [currentPageNumber, setCurrentPageNumber] = useState(1);
 
-  const handlePageSizeChange = (event) => {
-    setPageSize(Number(event.target.value)); n
-    setCurrentPage(1);
-  };
+    const totalPages = Math.ceil(comments.length / commentsPerPage);
+    const currentPageComments = comments.slice(
+        (currentPageNumber - 1) * commentsPerPage, currentPageNumber * commentsPerPage
+    );
 
-  const totalPages = Math.ceil(comments.length / pageSize);
-  const paginatedComments = comments.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+    const handleCommentsPerPageChange = (event) => {
+        setCommentsPerPage(Number(event.target.value));
+        setCurrentPageNumber(1);
+    };
 
+    const handlePrevPage = () => {
+        if (currentPageNumber > 1) {
+            setCurrentPageNumber(currentPageNumber - 1);
+        }
+    };
 
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
+    const handleNextPage = () => {
+        if (currentPageNumber < totalPages) {
+            setCurrentPageNumber(currentPageNumber + 1);
+        }
+    };
 
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-
-  return (
-    <>
-    <Flex
-      w="100%"
-        borderRadius={15}
-        borderWidth=".07em"
-        borderColor="#D2D2D2"
-        p={3}
-      >
-        <Table
-          variant="striped"
-          color="$EDF2F7"
-        >
-          <Thead>
-            <Tr>
-              <Th>Date</Th>
-              <Th>Comment</Th>
-              <Th>Amount</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {Array.isArray(comments) && comments.length > 0 ? (
-              paginatedComments.map((comment) => (
-                <Tr key={comment.id}>
-                  <Td>{format(new Date(comment.date), 'M/d/yy')}</Td>
-                  <Td>{comment.comment}</Td>
-                  <Td fontWeight="bold">{comment.amount}</Td>
-                </Tr>
-              ))
-            ) : (
-              <Tr>
-                <Td colSpan="3">No comments available.</Td>
-              </Tr>
-            )}
-
-          </Tbody>
-        </Table>
-
-      </Flex>
-
-      {/* pagination */}
-      <Flex w="100%" justifyContent="space-between">
-        <Flex align="center">
-          <Text mr={3}>Show: </Text>
-          <Select value={pageSize} onChange={handlePageSizeChange} width="auto">
-            <option value={3}>3</option>
-            <option value={5}>5</option>
-            <option value={10}>10</option>
-            <option value={20}>20</option>
-          </Select>
-          <Text ml={3}>per page</Text>
+    return (
+        <Flex direction="column" height={height} width={width} gap={gap}>
+            <Text fontWeight="bold" fontSize={headerSize}>
+                Comments
+            </Text>
+            <TableContainer
+                borderRadius="12px"
+                borderWidth="1px"
+                borderColor="#E2E8F0"
+            >
+                <Table
+                    variant="striped"
+                    color="#EDF2F7"
+                >
+                    <Thead>
+                        <Tr>
+                            <Th fontSize={textSize}> Date </Th>
+                            <Th fontSize={textSize}> Comment </Th>
+                            <Th fontSize={textSize}> Amount </Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody color="#2D3748">
+                        {Array.isArray(comments) && comments.length > 0 ? (
+                            currentPageComments.map((comment) => (
+                                <Tr key={comment.id}>
+                                    <Td fontSize={textSize}>
+                                        {format(new Date(comment.datetime), 'M/d/yy')}
+                                    </Td>
+                                    <Td fontSize={textSize}>
+                                        {comment.comment}
+                                    </Td>
+                                    <Td fontSize={textSize} fontWeight="bold">
+                                        ${Number(comment.adjustmentValue).toFixed(2)}
+                                    </Td>
+                                </Tr>))
+                        ) : (
+                            <Tr>
+                                <Td> No comments available. </Td>
+                            </Tr>
+                        )}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+            <Flex direction="row" width="100%" alignItems="center">
+                <Text fontSize={textSize} marginRight="0.5rem"> Show: </Text>
+                <Select width="auto" marginRight="0.5rem" value={commentsPerPage} onChange={handleCommentsPerPageChange}>
+                    <option value={3}>3</option>
+                    <option value={5}>5</option>
+                    <option value={10}>10</option>
+                    <option value={20}>20</option>
+                </Select>
+                <Text>per page</Text>
+                <Flex direction="row" marginLeft="auto" alignItems="center">
+                    <Text fontSize={textSize} marginRight="1rem"> {currentPageNumber} of {totalPages} </Text>
+                    <Button onClick={handlePrevPage} isDisabled={currentPageNumber === 1} borderLeftRadius={30}>
+                        <FaAngleLeft></FaAngleLeft>
+                    </Button>
+                    <Button onClick={handleNextPage} isDisabled={currentPageNumber === totalPages} borderRightRadius={30}>
+                        <FaAngleRight></FaAngleRight>
+                    </Button>
+                </Flex>
+            </Flex>
         </Flex>
-
-        <Flex align="center" >
-          <Text mr={3}>{currentPage} of {totalPages}</Text>
-          <Button onClick={handlePrevPage} isDisabled={currentPage === 1} borderLeftRadius={30} >
-            Prev
-          </Button>
-          <Button onClick={handleNextPage} isDisabled={currentPage === totalPages} borderRightRadius={30}>
-            Next
-          </Button>
-        </Flex>
-      </Flex>
+    );
+}
 
 
-</>
-  );
+// export const InvoicePayments = ({ comments }) => {
+//   const [pageSize, setPageSize] = useState(3);
+//   const [currentPage, setCurrentPage] = useState(1);
 
-};
+//   const handlePageSizeChange = (event) => {
+//     setPageSize(Number(event.target.value)); n
+//     setCurrentPage(1);
+//   };
+
+//   const totalPages = Math.ceil(comments.length / pageSize);
+//   const paginatedComments = comments.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+
+
+//   const handleNextPage = () => {
+//     if (currentPage < totalPages) {
+//       setCurrentPage(currentPage + 1);
+//     }
+//   };
+
+//   const handlePrevPage = () => {
+//     if (currentPage > 1) {
+//       setCurrentPage(currentPage - 1);
+//     }
+//   };
+
+
+//   return (
+//     <>
+//     <Flex
+//       w="100%"
+//         borderRadius={15}
+//         borderWidth=".07em"
+//         borderColor="#D2D2D2"
+//         p={3}
+//       >
+//         <Table
+//           variant="striped"
+//           color="$EDF2F7"
+//         >
+//           <Thead>
+//             <Tr>
+//               <Th>Date</Th>
+//               <Th>Comment</Th>
+//               <Th>Amount</Th>
+//             </Tr>
+//           </Thead>
+//           <Tbody>
+// {Array.isArray(comments) && comments.length > 0 ? (
+//   paginatedComments.map((comment) => (
+//     <Tr key={comment.id}>
+//       <Td>{format(new Date(comment.date), 'M/d/yy')}</Td>
+//       <Td>{comment.comment}</Td>
+//       <Td fontWeight="bold">{comment.amount}</Td>
+//     </Tr>
+//   ))
+// ) : (
+//   <Tr>
+//     <Td colSpan="3">No comments available.</Td>
+//   </Tr>
+// )}
+
+//           </Tbody>
+//         </Table>
+
+//       </Flex>
+
+//       {/* pagination */}
+//       <Flex w="100%" justifyContent="space-between">
+//         <Flex align="center">
+//           <Text mr={3}>Show: </Text>
+//           <Select value={pageSize} onChange={handlePageSizeChange} width="auto">
+//             <option value={3}>3</option>
+//             <option value={5}>5</option>
+//             <option value={10}>10</option>
+//             <option value={20}>20</option>
+//           </Select>
+//           <Text ml={3}>per page</Text>
+//         </Flex>
+
+//         <Flex align="center" >
+//           <Text mr={3}>{currentPage} of {totalPages}</Text>
+//   <Button onClick={handlePrevPage} isDisabled={currentPage === 1} borderLeftRadius={30} >
+//     Prev
+//   </Button>
+//   <Button onClick={handleNextPage} isDisabled={currentPage === totalPages} borderRightRadius={30}>
+//     Next
+//   </Button>
+//         </Flex>
+//       </Flex>
+
+
+// </>
+//   );
+// };
