3c0a3815dcd0c7e34511558f1c00078adb5b9051
Priska Kunaidy <kunaidyp@uci.edu>
2025-02-04 17:00:43 -0800

    5pm feb 4

--- client/src/components/programs/Program.jsx
+++ client/src/components/programs/Program.jsx
@@ -1,4 +1,4 @@
-
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { ProgramSummary, Sessions } from './ProgramComponents';
 import Navbar from '../Navbar';
@@ -7,17 +7,122 @@ import {
   Text,
   Flex,
 } from "@chakra-ui/react"
+import { useBackendContext } from '../../contexts/hooks/useBackendContext';
 
 export const Program = () => {
-  const {id}= useParams()
+  const { id } = useParams();
+  const { backend } = useBackendContext();
+  const [ program, setProgram ] = useState(null);
+  const [ sessions, setSessions ] = useState(null);
+  const [ roomIds, setRoomIds ] = useState(null);
+  const [ roomNames, setRoomNames] = useState(null);
+  const [ nextRoom, setNextRoom ] = useState(null);
+  const [ nextBookingInfo, setNextBookingInfo ] = useState(null);
+
+
+  const getProgram = async () => {
+    try {
+      const programResponse = await backend.get(`/events/${id}`);
+      const programData = programResponse.data;
+      setProgram(programData); // programData should have what Events table in DB model contains
+    } catch {
+      console.log("From getProgram: ", error);
+    }
+  };
+
+
+  const getNextRoom = async () => {
+    try {
+      const nextSession = sessions?.find(session => !session.archived);
+
+      if (!nextSession) {
+        console.log("No upcoming sessions found");
+        return;
+      }
+
+      const roomResponse = await backend.get(`/rooms/${nextSession.roomId}`);
+      setNextRoom(roomResponse.data[0]);
+    }catch {
+      console.log("From getNextRoom: ", error);
+    }
+
+  }
+
+  const getNextBookingInfo = async () => {
+    try {
+      const nextBooking = {
+        nextSession: {},
+        nextRoom:{},
+
+
+      };
+      const nextSession = sessions?.find(session => !session.archived);
+
+      if (!nextSession) {
+        console.log("No upcoming sessions found");
+        return;
+      }
+      nextBooking.nextSession = nextSessio
+      const roomResponse = await backend.get(`/rooms/${nextBooking.nextSession.roomId}`);
+      setNextRoom(roomResponse.data[0]);
+      nextBooking.nextRoom = roomResponse.data[0]
+
+
+      const assignmentResponse = await backend.get(`/assigments/event/${nextBooking.nextSession.id}`);
+
+
+
+
+  const getSessions = async () => {
+    try {
+      const sessionsResponse = await backend.get(`bookings/event/${id}`);
+      const sessionsData = sessionsResponse.data;
+      setSessions(sessionsData);
+      // Get set of room ids
+      const uniqueRoomIds = [...new Set(sessionsData.map(session => session.roomId))];
+      setRoomIds(uniqueRoomIds);
+
+    } catch {
+      console.log("From getSessions: ", error);
+    }
+  };
+
+  // Use set of room ids to create map of room id to name, pass map into sessions component
+  const getRoomNames = async () => {
+    try {
+      const roomMap = new Map();
+      for (const roomId of roomIds) {
+        const roomResponse = await backend.get(`rooms/${roomId}`);
+        const roomData = roomResponse.data;
+        roomMap.set(roomId, roomData[0].name);
+      }
+      setRoomNames(roomMap);
+    } catch {
+      console.log("From getRoomNames: ", error);
+    }
+  }
+
+  useEffect(() => {
+    getProgram();
+    getSessions();
+
+  }, [id]);
+
+  useEffect(() => {
+    if (roomIds) {
+      getRoomNames();
+      getNextRoom();
+    }
+  }, [roomIds]);
+
   return (
     <Flex>
       <Navbar/>
       <Box>
-        <ProgramSummary />
-        <Sessions />
+        <ProgramSummary program={program} nextRoom={nextRoom} sessions = {sessions}/>
+        <Sessions sessions={sessions} rooms={roomNames}/>
       </Box>
     </Flex>
-);
+  );
+};
 }
-
--- client/src/components/programs/ProgramComponents.jsx
+++ client/src/components/programs/ProgramComponents.jsx
@@ -28,6 +28,24 @@ import {
     useColorModeValue,
     Link,
     Slide,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+    useDisclosure,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    useBoolean,
+    Portal,
+    HStack
   } from "@chakra-ui/react"
 
   import {
@@ -48,158 +66,214 @@ import {
 
   import {FileTextIcon, EllipsisIcon, Calendar, MapPin, SlidersHorizontal, ChevronsUpDown} from 'lucide-react'
 
-export const ProgramSummary = () => {
+export const ProgramSummary = ({program, nextRoom, sessions}) => {
 
-    const [program, setProgram] = useState(null);
-    const { id } = useParams();
+  // Make sure program data is fetched before rendering
+  if (!program || program.length === 0) {
+    return <div>Loading...</div>; // Possibly change loading indicator
+  }
+  // console.log("programs", program[0])
+  // console.log("sessions: ",  nextRoom)
+  return (
+      <Box minH="10vh" py={8}>
+          <Container maxW="90%" >
+              <Card shadow="md" border="1px" borderColor="gray.300" borderRadius="15px">
+                  <CardBody m={6}>
+                      <Flex mb={6} justify="space-between" align="center" >
+                          <Flex align="center" gap={2}>
+                              <Flex align="center" gap={2}>
+                                  <Icon as={FileTextIcon} boxSize={6} color="gray.600" />
+                                  <Text fontSize="xl" fontWeight="semibold" color="gray.600">
+                                      Summary
+                                  </Text>
+                              </Flex>
+                          </Flex>
 
-    useEffect(() => {
+                          <Flex align="center" gap={2}>
+                              <Button leftIcon={<Icon as={DownloadIcon} />} colorScheme="purple" size="sm" borderRadius="20px">
+                                  Invoice
+                              </Button>
+                              <Menu>
+                                  <MenuButton as={IconButton} icon={<Icon as={EllipsisIcon} />} aria-label="Options" border="0.5px" bg="gray.50" size="sm" variant="ghost" borderRadius="20px"  />
+                                  <MenuList>
+                                      <MenuItem icon={<Icon as={EditIcon} />}>Edit</MenuItem>
+                                      <MenuItem icon={<Icon as={CloseIcon} />} color="red.500">
+                                          Cancel
+                                      </MenuItem>
+                                  </MenuList>
+                              </Menu>
+                          </Flex>
+                      </Flex>
 
-        const fetchProgram = async () => {
-            try{
-                const response = await fetch(`http://localhost:3001/events/${id}`);
-                console.log("REsponse: ", response);
+                      <Stack spacing={6}>
+                          <Heading as="h2" size="md" textColor="gray.600">
+                            {program[0].name}
+                          </Heading>
 
-                const data = await response.json();
-                console.log("Program data: ", data);
-                setProgram(data[0] || data);
-            }catch (err){
-                console.error(err);
-            }
-        }
-        if (id) {
-            fetchProgram();
-          }
+                          <Flex align="center" gap={2} color="gray.700">
+                              <Icon as={TimeIcon}/>
+                              <Text>10:00 am - 1:00 am</Text>
+                              <Text color="gray.600">next up on</Text>
+                              <Icon as={CalendarIcon} />
+                              <Text>Thu. 01/16/2025</Text>
+                          </Flex>
 
-      }, [id]);
+                          <Flex spacing={2} gap={6}>
+                              <Flex align="center" gap={2}>
+                              <Icon as={AtSignIcon} color="gray.600" />
+                              <Text color="gray.600">Aya de Leon, Jane Doe</Text>
+                              </Flex>
+                              <Flex align="center" gap={2}>
+                              <Icon as={AtSignIcon} color="gray.600" />
+                              <Text color="gray.600">Aya de Leon</Text>
+                              </Flex>
+                          </Flex>
 
+                          <Flex spacing={2} gap={6}>
+                              <Flex align="center" gap={2}>
+                              <Icon as={EmailIcon} color="gray.600" />
+                              <Text color="gray.600">AyadeLeon@gmail.com, JaneDoe@email.com</Text>
+                              </Flex>
+                          </Flex>
 
+                          <Flex  align="center" gap={8}>
+                              <Flex align="center" gap={2}>
+                                  <Icon as={InfoIcon} color="gray.600" />
+                                  <Text color="gray.600">Theater</Text>
+                              </Flex>
+                              <Flex align="center" gap={2}>
+                                  <Text color="gray.600">$</Text>
 
-    return (
-        <Box minH="10vh" py={8}>
-            <Container maxW="90%" >
-                <Card shadow="md" border="1px" borderColor="gray.300" borderRadius="15px">
-                    <CardBody m={6}>
-                        <Flex mb={6} justify="space-between" align="center" >
-                            <Flex align="center" gap={2}>
-                                <Flex align="center" gap={2}>
-                                    <Icon as={FileTextIcon} boxSize={6} color="gray.600" />
-                                    <Text fontSize="xl" fontWeight="semibold" color="gray.600">
-                                        Summary
-                                    </Text>
-                                </Flex>
-                            </Flex>
+                                  <Text color="gray.600">60.00</Text>
+                                  <Text color="gray.600">/ hour</Text>
+                              </Flex>
 
-                            <Flex align="center" gap={2}>
-                                <Button leftIcon={<Icon as={DownloadIcon} />} colorScheme="purple" size="sm" borderRadius="20px">
-                                    Invoice
-                                </Button>
-                                <Menu>
-                                    <MenuButton as={IconButton} icon={<Icon as={EllipsisIcon} />} aria-label="Options" border="0.5px" bg="gray.50" size="sm" variant="ghost" borderRadius="20px"  />
-                                    <MenuList>
-                                        <MenuItem icon={<Icon as={EditIcon} />}>Edit</MenuItem>
-                                        <MenuItem icon={<Icon as={CloseIcon} />} color="red.500">
-                                            Cancel
-                                        </MenuItem>
-                                    </MenuList>
-                                </Menu>
-                            </Flex>
-                        </Flex>
+                          </Flex>
 
-                        <Stack spacing={6}>
-                            <Heading as="h2" size="md" textColor="gray.600">
-                                Immigrant Rights Solidarity Week: Become an Immigration Rights Ambassador Workshop
-                            </Heading>
+                          <Stack spacing={6}>
+                              <Box spacing={2}>
+                                  <Heading size="md" textColor="gray.600">
+                                      Room Description
+                                  </Heading>
+                                  <Text color="gray.600" text="xs" mt={4}>
+                                  {program?.description || 'No description available'}
 
-                            <Flex align="center" gap={2} color="gray.700">
-                                <Icon as={TimeIcon}/>
-                                <Text>10:00 am - 1:00 am</Text>
-                                <Text color="gray.600">next up on</Text>
-                                <Icon as={CalendarIcon} />
-                                <Text>Thu. 01/16/2025</Text>
-                            </Flex>
+                                  </Text>
+                              </Box>
 
-                            <Flex spacing={2} gap={6}>
-                                <Flex align="center" gap={2}>
-                                <Icon as={AtSignIcon} color="gray.600" />
-                                <Text color="gray.600">Aya de Leon, Jane Doe</Text>
-                                </Flex>
-                                <Flex align="center" gap={2}>
-                                <Icon as={AtSignIcon} color="gray.600" />
-                                <Text color="gray.600">Aya de Leon</Text>
-                                </Flex>
-                            </Flex>
+                              <Box>
+                                  <Heading size="md" textColor="gray.600">
+                                      Class Description
+                                  </Heading>
+                                  <Text color="gray.600" text="xs" mt={4}>
+                                      Lorem ipsum dolor szt amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                                      et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                      culpa qui officia deserunt mollit anim id est laborum.
+                                  </Text>
+                              </Box>
 
-                            <Flex spacing={2} gap={6}>
-                                <Flex align="center" gap={2}>
-                                <Icon as={EmailIcon} color="gray.600" />
-                                <Text color="gray.600">AyadeLeon@gmail.com, JaneDoe@email.com</Text>
-                                </Flex>
-                            </Flex>
+                          </Stack>
 
-                            <Flex  align="center" gap={8}>
-                                <Flex align="center" gap={2}>
-                                    <Icon as={InfoIcon} color="gray.600" />
-                                    <Text color="gray.600">Theater</Text>
-                                </Flex>
-                                <Flex align="center" gap={2}>
-                                    <Text color="gray.600">$</Text>
+                      </Stack>
+                  </CardBody>
+              </Card>
 
-                                    <Text color="gray.600">60.00</Text>
-                                    <Text color="gray.600">/ hour</Text>
-                                </Flex>
+          </Container>
 
-                            </Flex>
-
-                            <Stack spacing={6}>
-                                <Box spacing={2}>
-                                    <Heading size="md" textColor="gray.600">
-                                        Room Description
-                                    </Heading>
-                                    <Text color="gray.600" text="xs" mt={4}>
-                                    {program?.description || 'No description available'}
-
-                                    </Text>
-                                </Box>
-
-                                <Box>
-                                    <Heading size="md" textColor="gray.600">
-                                        Class Description
-                                    </Heading>
-                                    <Text color="gray.600" text="xs" mt={4}>
-                                        Lorem ipsum dolor szt amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                        culpa qui officia deserunt mollit anim id est laborum.
-                                    </Text>
-                                </Box>
-
-                            </Stack>
-
-                        </Stack>
-                    </CardBody>
-                </Card>
-
-            </Container>
-
-        </Box>
-    );
+      </Box>
+  );
 };
 
-export const Sessions = () => {
+export const Sessions = ({ sessions, rooms }) => {
+  const formatDate = (isoString) => {
+    const date = new Date(isoString);
+
+
+    // Format the date to "Mon 01/17/2025"
+    const options = {
+      weekday: 'short',  // "Mon"
+      year: 'numeric',   // "2025"
+      month: '2-digit',  // "01"
+      day: '2-digit'
+    };
+    let formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+    formattedDate = formattedDate.replace(',', '.');
+    return formattedDate;
+  };
+
+  const formatTime = (timeString) => {
+    const [hours, minutes] = timeString.split(':').map(Number);
+
+    // Determine AM or PM suffix
+    const period = hours >= 12 ? 'p.m.' : 'a.m.';
+
+    // Convert to 12-hour format
+    const formattedHours = hours % 12 || 12; // Convert 0 to 12 for midnight
+
+    // Return formatted time
+    return `${formattedHours}:${minutes.toString().padStart(2, '0')} ${period}`;
+  };
+
+  const hasTimePassed = (dateTimeString) => {
+    // Function to determine color of status circle
+    const givenTime = new Date(dateTimeString);
+    const currentTime = new Date();
+    return currentTime > givenTime;
+  };
+
+  // Make sure sessions data is fetched before rendering
+  if (!sessions || sessions.length === 0) {
+    return <div>Loading...</div>; // Possibly change loading indicator
+  }
+  // Make sure rooms is fetched before rendering
+  if (!rooms || rooms.length === 0) {
+    return <div>Loading...</div>; // Possibly change loading indicator
+  }
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Box minH="10vh">
+    <Box minH="10vh" marginLeft="5vw" marginRight="5vw" marginBottom="4vw">
       <Card shadow="md" border="1px" borderColor="gray.300" borderRadius="15px">
         <CardBody m={6}>
           <Flex direction="column" justify="space-between">
             <Flex align="center" mb="15px">
-              <Icon as={Calendar} boxSize={5} color="#474849"/>
+              <Icon as={CalendarIcon} boxSize={5} color="#474849"/>
               <Text fontSize='xl' fontWeight="semibold" color="#474849" ml="8px"> Sessions </Text>
             </Flex>
             <Flex>
-
-              <Button color="#767778" variant="outline" size="sm" mb="15px" rounded="xl" leftIcon={<SlidersHorizontal />}> Filters </Button>
+              <Popover>
+                <PopoverTrigger>
+                <Button color="#767778" variant="outline" size="sm" mb="15px" rounded="xl" onClick={onOpen} leftIcon={<SlidersHorizontal />}> Filters </Button>
+                </PopoverTrigger>
+                <Portal>
+                  <PopoverContent>
+                    <PopoverBody>
+                      <FormControl id="date">
+                        <FormLabel>Date</FormLabel>
+                        <HStack>
+                          <Input size="xs" borderRadius="5px" width="30%" height="20%" placeholder="MM/DD/YYYY" />
+                          <Text> to </Text>
+                          <Input size="xs" borderRadius="5px" width="30%" height="20%" placeholder="MM/DD/YYYY" />
+                        </HStack>
+                      </FormControl>
+                      <FormControl id="time">
+                        <FormLabel>Time</FormLabel>
+                        <Input placeholder="MM/DD/YYYY" />
+                      </FormControl>
+                      <FormControl id="status">
+                        <FormLabel>Status</FormLabel>
+                        <Input placeholder="MM/DD/YYYY" />
+                      </FormControl>
+                      <FormControl id="room">
+                        <FormLabel>Room</FormLabel>
+                        <Input placeholder="MM/DD/YYYY" />
+                      </FormControl>
+                    </PopoverBody>
+                  </PopoverContent>
+                </Portal>
+              </Popover>
             </Flex>
             <TableContainer>
               <Table variant="unstyled">
@@ -213,20 +287,15 @@ export const Sessions = () => {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td><Box w="12px" h="12px" borderRadius="50%" bg="green" display="flex" align="center"/></Td>
-                    <Td>Thu. 01/16/2025</Td>
-                    <Td>10:00 a.m. - 1:00 p.m.</Td>
-                    <Td>Community</Td>
-                    <Td><IconButton size="md" rounded="full"><EllipsisIcon /></IconButton></Td>
-                  </Tr>
-                  <Tr>
-                    <Td><Box w="12px" h="12px" borderRadius="50%" bg="orange"/></Td>
-                    <Td>Thu. 01/16/2024</Td>
-                    <Td>10:00 a.m. - 1:00 p.m.</Td>
-                    <Td>Lounge</Td>
-                    <Td><IconButton size="md" rounded="full"><EllipsisIcon /></IconButton></Td>
-                  </Tr>
+                  {sessions.map((sesh) => (
+                    <Tr key={sesh.id}>
+                      <Td><Box height="10px" width="10px" borderRadius="50%" bg={hasTimePassed(sesh.date) ? "orange" : "green"}></Box></Td>
+                      <Td>{formatDate(sesh.date)}</Td>
+                      <Td>{formatTime(sesh.startTime)} - {formatTime(sesh.endTime)}</Td>
+                      <Td>{rooms.get(sesh.roomId)}</Td>
+                      <Td><IconButton size="sm" rounded="full"><EllipsisIcon /></IconButton></Td>
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
             </TableContainer>
