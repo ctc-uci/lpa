b5031bf42bd981323e7a10a3f5f51cac84b157c3
margoglvz <msgalvez06@gmail.com>
2025-02-16 02:33:49 -0800

    implemented midfi for passwordset

---
+++ client/src/components/forgotpassword/PasswordSet.jsx
@@ -0,0 +1,35 @@
+import {
+    Box,
+    Button,
+    Divider,
+    Heading,
+    VStack,
+  } from "@chakra-ui/react";
+  
+import logo from "../../assets/logo/logo.png";
+import { Link } from "react-router-dom";
+
+export const PasswordSet = () => {
+    return (
+        <VStack 
+            spacing={5}
+            sx={{ width: 550, marginX: "auto", mt:"44"}}
+        >
+            <Box as="img" src={logo} height="150px" width="280px" />
+            <Heading fontSize="40px" color={"#4E4AE7"}>Password is Set!</Heading>
+            <Divider h={"2px"}opacity="1" borderColor={"#4E4AE7"}/>
+            <Button 
+                as={Link} 
+                to="/login" 
+                size={"lg"}
+                mt={"30px"}
+                sx={{ width: "50%" }}
+                borderRadius={"25px"}
+                bg={"#4E4AE7"}
+                textColor={"white"}
+            > 
+                Back to Login
+            </Button>
+        </VStack>
+    );
+};
