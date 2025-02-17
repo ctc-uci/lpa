c10561f5e3b80d60e255f192776ca03977a360f6
Priska Kunaidy <kunaidyp@uci.edu>
2025-02-16 17:06:40 -0800

    completed the basic look of archived programs page

--- client/src/components/programs/ArchivedPrograms.jsx
+++ client/src/components/programs/ArchivedPrograms.jsx
@@ -15,6 +15,8 @@ import {
   IconButton,
   Input,
   InputGroup,
+  InputRightElement,
+  InputRightAddon,
   Menu,
   MenuButton,
   MenuItem,
@@ -51,6 +53,7 @@ import {
   archiveClock,
   archiveMapPin,
   archivePerson,
+  archiveMagnifyingGlass
 } from "../../assets/icons/ProgramIcons";
 
 export const ArchivedPrograms = () => {
@@ -66,13 +69,300 @@ export const ArchivedPrograms = () => {
                   Archived
                 </Text>
               </Flex>
-              <Box display="flex" justify-content="space-between" align-items="flex-start" align-self="stretch">
-                <Box marginRight="auto">
-                  <Text>Filters</Text>
-                </Box>
-                <Box>
-                  <Text>Search Bar</Text>
-                </Box>
+              <Box display="flex" justify-content="space-between" align-items="flex-start" align-self="stretch" marginTop="5px" marginBottom="15px">
+                <Flex marginRight="auto">
+                  <Popover>
+                    <PopoverTrigger>
+                      <Button
+                        backgroundColor="#F0F1F4"
+                        variant="subtle"
+                        minWidth="auto"
+                        height="40px"
+                        // mt="10px"
+                        // mb="15px"
+                        borderRadius="30px"
+                        // onClick={onOpen}
+                      >
+                        <Box
+                          display="flex"
+                          flexDirection="row"
+                          alignItems="center"
+                          gap="5px"
+                        >
+                          <Icon as={filterButton} />
+                          <Text
+                            fontSize="sm"
+                            color="#767778"
+                          >
+                            Filters
+                          </Text>
+                        </Box>
+                      </Button>
+                    </PopoverTrigger>
+                    <Portal>
+                      <PopoverContent>
+                        <Box margin="16px">
+                          <PopoverBody>
+                            <Box
+                              display="flex"
+                              flexDirection="column"
+                              alignItems="flex-start"
+                              gap="24px"
+                              alignSelf="stretch"
+                            >
+                              <FormControl id="date">
+                                <Box
+                                  display="flex"
+                                  flexDirection="column"
+                                  justifyContent="center"
+                                  alignItems="flex-start"
+                                  gap="16px"
+                                  alignSelf="stretch"
+                                >
+                                  <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    gap="5px"
+                                    alignSelf="stretch"
+                                  >
+                                    <Icon as={filterDateCalendar} />
+                                    <Text
+                                      fontWeight="bold"
+                                      color="#767778"
+                                    >
+                                      Date
+                                    </Text>
+                                  </Box>
+                                  <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    gap="8px"
+                                  >
+                                    <Input
+                                      size="sm"
+                                      borderRadius="5px"
+                                      borderColor="#D2D2D2"
+                                      backgroundColor="#F6F6F6"
+                                      width="35%"
+                                      height="20%"
+                                      type="date"
+                                      placeholder="MM/DD/YYYY"
+                                      onChange={(e) =>
+                                        handleDateChange("start", e.target.value)
+                                      }
+                                    />
+                                    <Text> to </Text>
+                                    <Input
+                                      size="sm"
+                                      borderRadius="5px"
+                                      borderColor="#D2D2D2"
+                                      backgroundColor="#F6F6F6"
+                                      width="35%"
+                                      height="20%"
+                                      type="date"
+                                      placeholder="MM/DD/YYYY"
+                                      onChange={(e) =>
+                                        handleDateChange("end", e.target.value)
+                                      }
+                                    />
+                                  </Box>
+                                </Box>
+                              </FormControl>
+                              <FormControl id="time">
+                                <Box
+                                  display="flex"
+                                  flexDirection="column"
+                                  justifyContent="center"
+                                  alignItems="flex-start"
+                                  gap="16px"
+                                  alignSelf="stretch"
+                                >
+                                  <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    gap="5px"
+                                    alignSelf="stretch"
+                                  >
+                                    <Icon as={sessionsFilterClock} />
+                                    <Text
+                                      fontWeight="bold"
+                                      color="#767778"
+                                    >
+                                      Time
+                                    </Text>
+                                  </Box>
+                                  <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    gap="8px"
+                                  >
+                                    <Input
+                                      size="xs"
+                                      borderRadius="5px"
+                                      borderColor="#D2D2D2"
+                                      backgroundColor="#F6F6F6"
+                                      width="30%"
+                                      height="20%"
+                                      type="time"
+                                      placeholder="00:00 am"
+                                      // onChange={(e) =>
+                                      //   handleTimeChange("start", e.target.value)
+                                      // }
+                                    />
+                                    <Text> to </Text>
+                                    <Input
+                                      size="xs"
+                                      borderRadius="5px"
+                                      borderColor="#D2D2D2"
+                                      backgroundColor="#F6F6F6"
+                                      width="30%"
+                                      height="20%"
+                                      type="time"
+                                      placeholder="00:00 pm"
+                                      // onChange={(e) =>
+                                      //   handleTimeChange("end", e.target.value)
+                                      // }
+                                    />
+                                  </Box>
+                                </Box>
+                              </FormControl>
+                              <FormControl id="status">
+                                <Box
+                                  display="flex"
+                                  flexDirection="column"
+                                  justifyContent="center"
+                                  alignItems="flex-start"
+                                  gap="16px"
+                                  alignSelf="stretch"
+                                >
+                                  <Text
+                                    fontWeight="bold"
+                                    color="#767778"
+                                  >
+                                    Status
+                                  </Text>
+                                  <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    gap="8px"
+                                  >
+                                    <Button
+                                      borderRadius="30px"
+                                      borderWidth="1px"
+                                      minWidth="auto"
+                                      height="20%"
+                                      // onClick={() => setStatus("All")}
+                                      // backgroundColor={
+                                      //   status === "All" ? "#EDEDFD" : "#F6F6F6"
+                                      // }
+                                      // borderColor={
+                                      //   status === "All" ? "#4E4AE7" : "#767778"
+                                      // }
+                                    >
+                                      All
+                                    </Button>
+                                    <Button
+                                      borderRadius="30px"
+                                      borderWidth="1px"
+                                      minWidth="auto"
+                                      height="20%"
+                                      // onClick={() => setStatus("Active")}
+                                      // backgroundColor={
+                                      //   status === "Active" ? "#EDEDFD" : "#F6F6F6"
+                                      // }
+                                      // borderColor={
+                                      //   status === "Active" ? "#4E4AE7" : "#767778"
+                                      // }
+                                    >
+                                      <Box
+                                        display="flex"
+                                        justifyContent="center"
+                                        alignItems="center"
+                                        gap="4px"
+                                      >
+                                        <Box
+                                          width="10px"
+                                          height="10px"
+                                          borderRadius="50%"
+                                          bg="#0C824D"
+                                        />
+                                        Active
+                                      </Box>
+                                    </Button>
+                                    <Button
+                                      borderRadius="30px"
+                                      borderWidth="1px"
+                                      minWidth="auto"
+                                      height="20%"
+                                      // onClick={() => setStatus("Past")}
+                                      // backgroundColor={
+                                      //   status === "Past" ? "#EDEDFD" : "#F6F6F6"
+                                      // }
+                                      // borderColor={
+                                      //   status === "Past" ? "#4E4AE7" : "#767778"
+                                      // }
+                                    >
+                                      <Box
+                                        display="flex"
+                                        justifyContent="center"
+                                        alignItems="center"
+                                        gap="4px"
+                                      >
+                                        <Box
+                                          width="10px"
+                                          height="10px"
+                                          borderRadius="50%"
+                                          bg="#DAB434"
+                                        />
+                                        Past
+                                      </Box>
+                                    </Button>
+                                  </Box>
+                                </Box>
+                              </FormControl>
+                              <FormControl id="room">
+                                <Box
+                                  display="flex"
+                                  flexDirection="column"
+                                  justifyContent="center"
+                                  alignItems="flex-start"
+                                  gap="16px"
+                                  alignSelf="stretch"
+                                >
+                                  <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    gap="5px"
+                                    alignSelf="stretch"
+                                  >
+                                    <Icon as={sessionsFilterMapPin} />
+                                    <Text
+                                      fontWeight="bold"
+                                      color="#767778"
+                                    >
+                                      Room
+                                    </Text>
+                                  </Box>
+
+                                </Box>
+                              </FormControl>
+                            </Box>
+                          </PopoverBody>
+                        </Box>
+                      </PopoverContent>
+                    </Portal>
+                  </Popover>
+                </Flex>
+                <Flex>
+                  <InputGroup size="md" width="300px" variant="outline" borderColor="#D2D2D2" background="white" type="text">
+                    <Input placeholder="Search..." borderRadius="15px" />
+                    <InputRightElement marginRight="4px">
+                      <Button size="sm" borderRadius="15px" background="#F0F1F4" onClick={() => console.log("Search clicked")}>
+                        <Icon as={archiveMagnifyingGlass} />
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </Flex>
               </Box>
               <TableContainer>
                 <Table variant="unstyled">
@@ -135,6 +425,9 @@ export const ArchivedPrograms = () => {
                             Payer(s)
                           </Text>
                         </Box>
+                      </Th>
+                      <Th>
+                        {/* Empty column for ellipsis button */}
                       </Th>
                     </Tr>
                   </Thead>
