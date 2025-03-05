de5836c48bca2a7e9d2984d72bb2c07dfdee0efe
margoglvz <msgalvez06@gmail.com>
2025-03-04 21:42:06 -0800

    updated icon features

--- client/src/assets/CloseFilledIcon.jsx
+++ client/src/assets/CloseFilledIcon.jsx
@@ -4,9 +4,9 @@ export const CloseFilledIcon = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
       <rect width="24" height="24" rx="12" fill="#767778"/> 
-      <path fill-rule="evenodd" clip-rule="evenodd" 
+      <path fillRule="evenodd" clipRule="evenodd" 
         d="M8 8 L16 16 M16 8 L8 16" 
-        stroke="white" stroke-width="2" stroke-linecap="round"/>
+        stroke="white" strokeWidth="2" strokeLinecap="round"/>
     </svg>
 
   );
