bf0091ab72797355be122fc0452a80cd41076c81
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2025-03-05 23:09:16 -0800

    use LEFT JOIN in case payee is empty

--- server/routes/invoicesAssignments.js
+++ server/routes/invoicesAssignments.js
@@ -9,15 +9,15 @@ invoicesAssignments.get("/", async (req, res) => {
   try {
     const invoices = await db.query(
       `SELECT events.name as event_name, invoices.is_sent, invoices.payment_status, clients.name, invoices.end_date, assignments.role
-       FROM events 
-       JOIN invoices ON events.id = invoices.event_id 
-       JOIN assignments ON assignments.event_id = events.id
-       JOIN clients ON clients.id = assignments.client_id;`, 
+       FROM events
+       JOIN invoices ON events.id = invoices.event_id
+       LEFT JOIN assignments ON assignments.event_id = events.id
+       LEFT JOIN clients ON clients.id = assignments.client_id;`,
     );
 
 
     res.status(200).json(keysToCamel(invoices)); // Ensure you return `data.rows`
-  } 
+  }
   catch (err) {
     res.status(500).send(err.message); // Fixed error response
   }
