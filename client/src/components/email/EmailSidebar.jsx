f28c1d855edac605218f5c2841cb92db9f0a76be
Brendan Lieu <brendanlieu05@gmail.com>
2025-03-25 18:59:28 -0700

    Prefilled To field with payees

--- client/src/components/email/EmailSidebar.jsx
+++ client/src/components/email/EmailSidebar.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 import {
   Box,
@@ -20,30 +20,61 @@ import {
   InputRightElement,
   SimpleGrid,
   Stack,
+  Tag,
+  TagCloseButton,
+  TagLabel,
   Text,
   VStack,
-  Tag,
-  TagLabel,
-  TagCloseButton
 } from "@chakra-ui/react";
 
 import IoPaperPlane from "../../assets/IoPaperPlane.svg";
 import logo from "../../assets/logo/logo.png";
 import { DiscardEmailModal } from "./DiscardEmailModal";
 import { EmailConfirmationModal } from "./EmailConfirmationModal";
+import { useBackendContext } from "../../contexts/hooks/useBackendContext";
+import { useParams } from "react-router-dom";
 
 export const EmailSidebar = ({ isOpen, onOpen, onClose }) => {
+  const { backend } = useBackendContext();
+  const { id } = useParams();
+
   const [toInput, setToInput] = useState("");
-  const [emails, setEmails] = useState([]);
   const [ccInput, setCcInput] = useState("");
   const [bccInput, setBccInput] = useState("");
+  const [emails, setEmails] = useState(new Set());
   const [ccEmails, setCcEmails] = useState([]);
   const [bccEmails, setBccEmails] = useState([]);
+  
+
   const btnRef = useRef();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const payeesResponse = await backend.get("/invoices/payees/" + id);
+        const payeeEmails = payeesResponse.data.map((payee) => payee.email);
+        
+        // Directly add the emails to the set
+        setEmails((prevEmails) => {
+          const updatedEmails = new Set(prevEmails);
+          payeeEmails.forEach(email => updatedEmails.add(email)); // Add each email to the Set
+          return updatedEmails;
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(emails);
+  }, [emails])
+
   const handleAddEmail = () => {
-    if (toInput && !emails.includes(toInput)) {
-      setEmails([...emails, toInput]);
+    if (toInput.includes("@") && toInput.includes(".") && !emails.has(toInput)) {
+      setEmails(new Set(emails.add(toInput)));
       setToInput(""); // Clear the input after adding
     }
   };
@@ -55,25 +86,25 @@ export const EmailSidebar = ({ isOpen, onOpen, onClose }) => {
   };
 
   const handleAddCcEmail = () => {
-    if (ccInput && !ccEmails.includes(ccInput)) {
+    if (ccInput.includes("@") && ccInput.includes(".") && !ccEmails.includes(ccInput)) {
       setCcEmails((prev) => [...prev, ccInput]);
       setCcInput("");
     }
   };
-  
+
   const handleAddBccEmail = () => {
-    if (bccInput && !bccEmails.includes(bccInput)) {
+    if (bccInput.includes("@") && bccInput.includes(".") && !bccEmails.includes(bccInput)) {
       setBccEmails((prev) => [...prev, bccInput]);
       setBccInput("");
     }
   };
-  
+
   const handleKeyPressCc = (e) => {
     if (e.key === "Enter") {
       handleAddCcEmail();
     }
   };
-  
+
   const handleKeyPressBcc = (e) => {
     if (e.key === "Enter") {
       handleAddBccEmail();
@@ -154,7 +185,12 @@ export const EmailSidebar = ({ isOpen, onOpen, onClose }) => {
                   To:
                 </Text>
                 <InputGroup w="200px">
-                  <Input placeholder="johndoe@gmail.com" onChange={(e) => setToInput(e.target.value)} value={toInput} onKeyDown={handleKeyDown}/>
+                  <Input
+                    placeholder="johndoe@gmail.com"
+                    onChange={(e) => setToInput(e.target.value)}
+                    value={toInput}
+                    onKeyDown={handleKeyDown}
+                  />
                   <InputRightElement>
                     <IconButton
                       bgColor="transparent"
@@ -191,100 +227,172 @@ export const EmailSidebar = ({ isOpen, onOpen, onClose }) => {
                   </InputRightElement>
                 </InputGroup>
               </Flex>
-              <Flex gap="4" justifyContent="start" wrap='wrap'>
-                  {emails.map((email, index) => (
-                    <Tag key={index} size="lg" borderRadius="full" variant="solid">
-                      <TagLabel>{email}</TagLabel>
-                      <TagCloseButton onClick={() => setEmails(emails.filter((e) => e !== email))} />
-                    </Tag>
-                  ))}
-                </Flex>
-
-                <Flex justifyContent="space-between" alignItems="center">
-                  <Text color="#474849" fontSize="14px">CC:</Text>
-                  <InputGroup w="200px">
-                    <Input
-                      placeholder="johndoe@gmail.com"
-                      value={ccInput}
-                      onChange={(e) => setCcInput(e.target.value)}
-                      onKeyDown={handleKeyPressCc}
+              <Flex gap="4" justifyContent="start" wrap="wrap">
+                {[...emails].map((email, index) => (
+                  <Tag key={index} size="lg" borderRadius="full" variant="solid">
+                    <TagLabel>{email}</TagLabel>
+                    <TagCloseButton
+                      onClick={() =>
+                        setEmails((prevEmails) => {
+                          const updatedEmails = new Set(prevEmails);
+                          updatedEmails.delete(email);
+                          return updatedEmails;
+                        })
+                      }
                     />
-                    <InputRightElement>
-                      <IconButton
-                        bgColor="transparent"
-                        _hover={{
-                          bgColor: "transparent",
-                          svg: {
-                            rect: { fill: "#4441C8" },
-                          },
-                        }}
-                        _active={{ bgColor: "transparent" }}
-                        _focus={{ boxShadow: "none" }}
-                        onClick={handleAddCcEmail}
-                        icon={
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 20" fill="none">
-                            <rect width="20" height="20" rx="10" fill="#718096" />
-                            <path
-                              d="M9.16675 10.8333H5.00008C4.76397 10.8333 4.5662 10.7533 4.40675 10.5933C4.24731 10.4333 4.16731 10.2355 4.16675 9.99996C4.1662 9.76441 4.2462 9.56663 4.40675 9.40663C4.56731 9.24663 4.76508 9.16663 5.00008 9.16663H9.16675V4.99996C9.16675 4.76385 9.24675 4.56607 9.40675 4.40663C9.56675 4.24718 9.76453 4.16718 10.0001 4.16663C10.2356 4.16607 10.4337 4.24607 10.5943 4.40663C10.7548 4.56718 10.8345 4.76496 10.8334 4.99996V9.16663H15.0001C15.2362 9.16663 15.4343 9.24663 15.5943 9.40663C15.7543 9.56663 15.834 9.76441 15.8334 9.99996C15.8329 10.2355 15.7529 10.4336 15.5934 10.5941C15.434 10.7547 15.2362 10.8344 15.0001 10.8333H10.8334V15C10.8334 15.2361 10.7534 15.4341 10.5934 15.5941C10.4334 15.7541 10.2356 15.8339 10.0001 15.8333C9.76453 15.8327 9.56675 15.7527 9.40675 15.5933C9.24675 15.4339 9.16675 15.2361 9.16675 15V10.8333Z"
-                              fill="white"
-                            />
-                          </svg>
-                        }
-                      />
-                    </InputRightElement>
-                  </InputGroup>
-                </Flex>
-                <Flex gap="4" justifyContent="start" wrap='wrap'>
-                  {ccEmails.map((email, index) => (
-                    <Tag key={index} size="lg" borderRadius="full" variant="solid">
-                      <TagLabel>{email}</TagLabel>
-                      <TagCloseButton onClick={() => setCcEmails(ccEmails.filter((e) => e !== email))} />
-                    </Tag>
-                  ))}
-                </Flex>
+                  </Tag>
+                ))}
+              </Flex>
 
-                <Flex justifyContent="space-between" alignItems="center">
-                  <Text color="#474849" fontSize="14px">BCC:</Text>
-                  <InputGroup w="200px">
-                    <Input
-                      placeholder="johndoe@gmail.com"
-                      value={bccInput}
-                      onChange={(e) => setBccInput(e.target.value)}
-                      onKeyDown={handleKeyPressBcc}
+              <Flex
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Text
+                  color="#474849"
+                  fontSize="14px"
+                >
+                  CC:
+                </Text>
+                <InputGroup w="200px">
+                  <Input
+                    placeholder="johndoe@gmail.com"
+                    value={ccInput}
+                    onChange={(e) => setCcInput(e.target.value)}
+                    onKeyDown={handleKeyPressCc}
+                  />
+                  <InputRightElement>
+                    <IconButton
+                      bgColor="transparent"
+                      _hover={{
+                        bgColor: "transparent",
+                        svg: {
+                          rect: { fill: "#4441C8" },
+                        },
+                      }}
+                      _active={{ bgColor: "transparent" }}
+                      _focus={{ boxShadow: "none" }}
+                      onClick={handleAddCcEmail}
+                      icon={
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <rect
+                            width="20"
+                            height="20"
+                            rx="10"
+                            fill="#718096"
+                          />
+                          <path
+                            d="M9.16675 10.8333H5.00008C4.76397 10.8333 4.5662 10.7533 4.40675 10.5933C4.24731 10.4333 4.16731 10.2355 4.16675 9.99996C4.1662 9.76441 4.2462 9.56663 4.40675 9.40663C4.56731 9.24663 4.76508 9.16663 5.00008 9.16663H9.16675V4.99996C9.16675 4.76385 9.24675 4.56607 9.40675 4.40663C9.56675 4.24718 9.76453 4.16718 10.0001 4.16663C10.2356 4.16607 10.4337 4.24607 10.5943 4.40663C10.7548 4.56718 10.8345 4.76496 10.8334 4.99996V9.16663H15.0001C15.2362 9.16663 15.4343 9.24663 15.5943 9.40663C15.7543 9.56663 15.834 9.76441 15.8334 9.99996C15.8329 10.2355 15.7529 10.4336 15.5934 10.5941C15.434 10.7547 15.2362 10.8344 15.0001 10.8333H10.8334V15C10.8334 15.2361 10.7534 15.4341 10.5934 15.5941C10.4334 15.7541 10.2356 15.8339 10.0001 15.8333C9.76453 15.8327 9.56675 15.7527 9.40675 15.5933C9.24675 15.4339 9.16675 15.2361 9.16675 15V10.8333Z"
+                            fill="white"
+                          />
+                        </svg>
+                      }
                     />
-                    <InputRightElement>
-                      <IconButton
-                        bgColor="transparent"
-                        _hover={{
-                          bgColor: "transparent",
-                          svg: {
-                            rect: { fill: "#4441C8" },
-                          },
-                        }}
-                        _active={{ bgColor: "transparent" }}
-                        _focus={{ boxShadow: "none" }}
-                        onClick={handleAddBccEmail}
-                        icon={
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 20" fill="none">
-                            <rect width="20" height="20" rx="10" fill="#718096" />
-                            <path
-                              d="M9.16675 10.8333H5.00008C4.76397 10.8333 4.5662 10.7533 4.40675 10.5933C4.24731 10.4333 4.16731 10.2355 4.16675 9.99996C4.1662 9.76441 4.2462 9.56663 4.40675 9.40663C4.56731 9.24663 4.76508 9.16663 5.00008 9.16663H9.16675V4.99996C9.16675 4.76385 9.24675 4.56607 9.40675 4.40663C9.56675 4.24718 9.76453 4.16718 10.0001 4.16663C10.2356 4.16607 10.4337 4.24607 10.5943 4.40663C10.7548 4.56718 10.8345 4.76496 10.8334 4.99996V9.16663H15.0001C15.2362 9.16663 15.4343 9.24663 15.5943 9.40663C15.7543 9.56663 15.834 9.76441 15.8334 9.99996C15.8329 10.2355 15.7529 10.4336 15.5934 10.5941C15.434 10.7547 15.2362 10.8344 15.0001 10.8333H10.8334V15C10.8334 15.2361 10.7534 15.4341 10.5934 15.5941C10.4334 15.7541 10.2356 15.8339 10.0001 15.8333C9.76453 15.8327 9.56675 15.7527 9.40675 15.5933C9.24675 15.4339 9.16675 15.2361 9.16675 15V10.8333Z"
-                              fill="white"
-                            />
-                          </svg>
-                        }
-                      />
-                    </InputRightElement>
-                  </InputGroup>
-                </Flex>
-                <Flex gap="4" justifyContent="start" wrap='wrap'>
-                  {bccEmails.map((email, index) => (
-                    <Tag key={index} size="lg" borderRadius="full" variant="solid">
-                      <TagLabel>{email}</TagLabel>
-                      <TagCloseButton onClick={() => setBccEmails(bccEmails.filter((e) => e !== email))} />
-                    </Tag>
-                  ))}
-                </Flex>
+                  </InputRightElement>
+                </InputGroup>
+              </Flex>
+              <Flex
+                gap="4"
+                justifyContent="start"
+                wrap="wrap"
+              >
+                {ccEmails.map((email, index) => (
+                  <Tag
+                    key={index}
+                    size="lg"
+                    borderRadius="full"
+                    variant="solid"
+                  >
+                    <TagLabel>{email}</TagLabel>
+                    <TagCloseButton
+                      onClick={() =>
+                        setCcEmails(ccEmails.filter((e) => e !== email))
+                      }
+                    />
+                  </Tag>
+                ))}
+              </Flex>
+
+              <Flex
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Text
+                  color="#474849"
+                  fontSize="14px"
+                >
+                  BCC:
+                </Text>
+                <InputGroup w="200px">
+                  <Input
+                    placeholder="johndoe@gmail.com"
+                    value={bccInput}
+                    onChange={(e) => setBccInput(e.target.value)}
+                    onKeyDown={handleKeyPressBcc}
+                  />
+                  <InputRightElement>
+                    <IconButton
+                      bgColor="transparent"
+                      _hover={{
+                        bgColor: "transparent",
+                        svg: {
+                          rect: { fill: "#4441C8" },
+                        },
+                      }}
+                      _active={{ bgColor: "transparent" }}
+                      _focus={{ boxShadow: "none" }}
+                      onClick={handleAddBccEmail}
+                      icon={
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                        >
+                          <rect
+                            width="20"
+                            height="20"
+                            rx="10"
+                            fill="#718096"
+                          />
+                          <path
+                            d="M9.16675 10.8333H5.00008C4.76397 10.8333 4.5662 10.7533 4.40675 10.5933C4.24731 10.4333 4.16731 10.2355 4.16675 9.99996C4.1662 9.76441 4.2462 9.56663 4.40675 9.40663C4.56731 9.24663 4.76508 9.16663 5.00008 9.16663H9.16675V4.99996C9.16675 4.76385 9.24675 4.56607 9.40675 4.40663C9.56675 4.24718 9.76453 4.16718 10.0001 4.16663C10.2356 4.16607 10.4337 4.24607 10.5943 4.40663C10.7548 4.56718 10.8345 4.76496 10.8334 4.99996V9.16663H15.0001C15.2362 9.16663 15.4343 9.24663 15.5943 9.40663C15.7543 9.56663 15.834 9.76441 15.8334 9.99996C15.8329 10.2355 15.7529 10.4336 15.5934 10.5941C15.434 10.7547 15.2362 10.8344 15.0001 10.8333H10.8334V15C10.8334 15.2361 10.7534 15.4341 10.5934 15.5941C10.4334 15.7541 10.2356 15.8339 10.0001 15.8333C9.76453 15.8327 9.56675 15.7527 9.40675 15.5933C9.24675 15.4339 9.16675 15.2361 9.16675 15V10.8333Z"
+                            fill="white"
+                          />
+                        </svg>
+                      }
+                    />
+                  </InputRightElement>
+                </InputGroup>
+              </Flex>
+              <Flex
+                gap="4"
+                justifyContent="start"
+                wrap="wrap"
+              >
+                {bccEmails.map((email, index) => (
+                  <Tag
+                    key={index}
+                    size="lg"
+                    borderRadius="full"
+                    variant="solid"
+                  >
+                    <TagLabel>{email}</TagLabel>
+                    <TagCloseButton
+                      onClick={() =>
+                        setBccEmails(bccEmails.filter((e) => e !== email))
+                      }
+                    />
+                  </Tag>
+                ))}
+              </Flex>
             </Stack>
 
             <Divider color="#E2E8F0" />
