9c9d4b73ce76796523f82b6122ad6240fb445249
Cole Thompson <colet0227@gmail.com>
2025-03-26 18:59:25 -0700

    feat: ensure create account page matches figma EXACTLY

--- client/src/components/signup/Signup.jsx
+++ client/src/components/signup/Signup.jsx
@@ -1,47 +1,42 @@
 import { useEffect, useState } from "react";
-
 import {
   Box,
   Button,
-  Divider,
   Link as ChakraLink,
   FormControl,
   FormErrorMessage,
   Heading,
+  HStack,
+  Icon,
   Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
   Stack,
+  Text,
   useToast,
   VStack,
-  Text,
-  HStack,
 } from "@chakra-ui/react";
-
-import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
-import logo from "../../assets/logo/logo.png";
-import programSvg from "../../assets/icons/program.svg";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { AiFillEye, AiFillLock, AiFillMail } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
 import { z } from "zod";
 
+import logo from "../../assets/logo/logo.png";
 import { useAuthContext } from "../../contexts/hooks/useAuthContext";
 import { useBackendContext } from "../../contexts/hooks/useBackendContext";
+import "./Signup.css";
 
-const signupSchema = z.object({
-  first_name: z.string("Invalid first name").min(1, { message: "First name required" }),
-  last_name: z.string("Invalid last name").min(1, { message: "Last name required" }),
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
-  confirmPassword: z.string().min(6, "Confirm Password must be at least 6 characters long"),
-})
-.refine((data) => data.password === data.confirmPassword, {
-  message: "Passwords do not match",
-  path: ["confirmPassword"],
-});
+const signupSchema = z
+  .object({
+    first_name: z.string("Invalid first name").min(1, { message: "First name required" }),
+    last_name: z.string("Invalid last name").min(1, { message: "Last name required" }),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    confirmPassword: z.string().min(6, "Confirm Password must be at least 6 characters long"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 export const Signup = () => {
   const navigate = useNavigate();
@@ -52,11 +47,7 @@ export const Signup = () => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
+  const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: zodResolver(signupSchema),
     mode: "onBlur",
   });
@@ -68,9 +59,7 @@ export const Signup = () => {
         password: data.password,
         first_name: data.first_name,
         last_name: data.last_name,
-
       });
-
       if (user) {
         navigate("/signup/requested");
       }
@@ -91,212 +80,175 @@ export const Signup = () => {
   }, [backend, handleRedirectResult, navigate, toast]);
 
   return (
-    <VStack
-      spacing={5}
-      sx={{ width: 550, marginX: "auto", mt:"20"}}
-    >
-      <Box
-        as="img"
-        src={logo}
-        height={"120px"}
-        width={"230px"}
-      />
-      <Heading fontFamily="'Inter', sans-serif" color={"#4E4AE7"}>Create Account</Heading>
-      <Divider h={"2px"} mt={-"10px"} opacity="1" borderColor={"#4E4AE7"} w="60%"/>
-      <Text fontSize={"17px"}>Please enter your information</Text>
-      <form
-        onSubmit={handleSubmit(handleSignup)}
-        style={{ width: "100%" }}
-      >
+    <VStack spacing={5} className="signup-container">
+      <Box as="img" src={logo} className="logo" />
+      <div className="header-container">
+        <Heading className="create-account-heading">Create Account</Heading>
+        <Text className="account-info-text">
+          Please enter your account information.
+        </Text>
+      </div>
+      <form onSubmit={handleSubmit(handleSignup)} className="signup-form">
         <Stack spacing={2}>
-        <HStack align="start">
-          <VStack align="start" w="100%">
-            <FormControl
-              isInvalid={!!errors.first_name}
-              w={"100%"}
-            >
-              <label htmlFor="first">First name</label>
-              <InputGroup>
-                <InputLeftElement 
-                  pointerEvents="none"
-                  display="flex"
-                  alignItems="center"
-                  h="100%"
-                >
-                  <Box as="img" src={programSvg} boxSize="20px" />
-                </InputLeftElement>
-                <Input
-                  placeholder="First name"
-                  type="text"
-                  borderRadius={"10px"}
-                  pl={-"4px"}
-                  size={"lg"}
-                  {...register("first_name")}
-                  name="first_name"
-                  isRequired
-                />
-              </InputGroup>
-              <FormErrorMessage minH="20px">
-                {errors.first_name?.message?.toString()}
+          {/* Name Fields */}
+          <div className="name-fields">
+            <HStack align="start" style={{ width: "363px", justifyContent: "space-between" }}>
+              <VStack align="flex-start" w="173px">
+                <FormControl isInvalid={!!errors.first_name}>
+                  <div className="name-field-container">
+                    <label htmlFor="first" className="form-label">First Name</label>
+                    <div className="name-input-outer">
+                      <div className="name-input-container">
+                        <Input
+                          id="first"
+                          placeholder="First name"
+                          type="text"
+                          variant="unstyled"
+                          className="name-input-text"
+                          {...register("first_name")}
+                          isRequired
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <FormErrorMessage className="form-error">
+                    {errors.first_name?.message?.toString()}
+                  </FormErrorMessage>
+                </FormControl>
+              </VStack>
+
+              <VStack align="flex-start" w="173px">
+                <FormControl isInvalid={!!errors.last_name}>
+                  <div className="name-field-container">
+                    <label htmlFor="last" className="form-label">Last Name</label>
+                    <div className="name-input-outer">
+                      <div className="name-input-container">
+                        <Input
+                          id="last"
+                          placeholder="Last name"
+                          type="text"
+                          variant="unstyled"
+                          className="name-input-text"
+                          {...register("last_name")}
+                          isRequired
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <FormErrorMessage className="form-error">
+                    {errors.last_name?.message?.toString()}
+                  </FormErrorMessage>
+                </FormControl>
+              </VStack>
+            </HStack>
+          </div>
+
+          {/* Fields Container */}
+          <div className="fields-container">
+            {/* Email Field */}
+            <FormControl isInvalid={!!errors.email} className="form-field-container">
+              <label htmlFor="email" className="form-label">Email</label>
+              <div className="input-outer">
+                <div className="input-icon-container">
+                  <Icon as={AiFillMail} boxSize="22px" color="#718096" />
+                </div>
+                <div className="input-text-container">
+                  <Input
+                    id="email"
+                    placeholder="Your email"
+                    type="text"
+                    variant="unstyled"
+                    className="input-text"
+                    {...register("email")}
+                    isRequired
+                    autoComplete="email"
+                  />
+                </div>
+                <div className="input-right-icon-container">
+                  {/* No right icon for email */}
+                </div>
+              </div>
+              <FormErrorMessage className="form-error">
+                {errors.email?.message?.toString()}
               </FormErrorMessage>
             </FormControl>
-          </VStack>
-          <VStack align="start" w="100%">
-            <FormControl
-              isInvalid={!!errors.last_name}
-              w={"100%"}
-            >
-              <label htmlFor="last">Last name</label>
-              <InputGroup>
-                <InputLeftElement 
-                  pointerEvents="none"
-                  display="flex"
-                  alignItems="center"
-                  h="100%"
-                >
-                  <Box as="img" src={programSvg} boxSize="20px" />
-                </InputLeftElement>
-              
-                <Input
-                  placeholder="Last name"
-                  type="text"
-                  size={"lg"}
-                  borderRadius={"10px"}
-                  {...register("last_name")}
-                  name="last_name"
-                  isRequired
-                />
-              </InputGroup>
-              <FormErrorMessage minH="20px">
-                {errors.last_name?.message?.toString()}
+
+            {/* Password Field */}
+            <FormControl isInvalid={!!errors.password} className="form-field-container">
+              <label htmlFor="password" className="form-label">Password</label>
+              <div className="input-outer">
+                <div className="input-icon-container">
+                  <Icon as={AiFillLock} boxSize="24px" color="#718096" />
+                </div>
+                <div className="input-text-container">
+                  <Input
+                    id="password"
+                    placeholder="Your password"
+                    type={isPasswordVisible ? "text" : "password"}
+                    variant="unstyled"
+                    className="input-text"
+                    {...register("password")}
+                    isRequired
+                    autoComplete="password"
+                  />
+                </div>
+                <div className="input-right-icon-container">
+                  <button
+                    type="button"
+                    onClick={() => setIsPasswordVisible(prev => !prev)}
+                    className="icon-button"
+                  >
+                    <Icon as={AiFillEye} boxSize="24px" color="#718096" />
+                  </button>
+                </div>
+              </div>
+              <FormErrorMessage className="form-error">
+                {errors.password?.message?.toString()}
               </FormErrorMessage>
             </FormControl>
-          </VStack>
-        </HStack>
-          <FormControl
-            isInvalid={!!errors.email}
-            w={"100%"}
-          >
-            <label htmlFor="email">Email</label>
-            <InputGroup>
-              <InputLeftElement 
-                pointerEvents="none"
-                display="flex"
-                alignItems="center"
-                h="100%"
-              >
-                <Box as="img" src={programSvg} boxSize="20px" />
-              </InputLeftElement>
-              <Input
-                placeholder="Email"
-                type="text"
-                size={"lg"}
-                borderRadius={"10px"}
-                {...register("email")}
-                name="email"
-                isRequired
-                autoComplete="email"
-              />
-            </InputGroup>
-            <FormErrorMessage>
-              {errors.email?.message?.toString()}
-            </FormErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={!!errors.password}>
-          <label htmlFor="password">Password</label>
-          <InputGroup>
-            <InputLeftElement 
-              pointerEvents="none"
-              display="flex"
-              alignItems="center"
-              h="100%"
-            >
-              <Box as="img" src={programSvg} boxSize="20px" />
-            </InputLeftElement>
-            <Input
-              placeholder="Password"
-              type={isPasswordVisible ? 'text' : 'password'}
-              size={"lg"}
-              borderRadius={"10px"}
-              {...register("password")}
-              name="password"
-              isRequired
-              autoComplete="password"
-            />
-            <InputRightElement 
-              display="flex"
-              alignItems="center"
-              h="100%"
-            >
-              <button onClick={() => {setIsPasswordVisible((prev) => !prev)}} style={{ background: 'none', border: 'none' }}>
-                {isPasswordVisible ? <AiOutlineEyeInvisible color="gray" /> : <AiOutlineEye color="gray" />}
-              </button>
-            </InputRightElement>
-          </InputGroup>
-            <FormErrorMessage>
-              {errors.password?.message?.toString()}
-            </FormErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={!!errors.confirmPassword}>
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <InputGroup>
-            <InputLeftElement 
-              pointerEvents="none"
-              display="flex"
-              alignItems="center"
-              h="100%"
-            >
-              <Box as="img" src={programSvg} boxSize="20px" />
-            </InputLeftElement>
-            <Input
-              placeholder="Confirm Password"
-              type={isConfirmPasswordVisible ? 'text' : 'password'}
-              size={"lg"}
-              borderRadius={"10px"}
-              {...register("confirmPassword")}
-              name="confirmPassword"
-              isRequired
-              autoComplete="password"
-            />
-            <InputRightElement 
-              display="flex"
-              alignItems="center"
-              h="100%"
-            >
-              <button onClick={() => {setIsConfirmPasswordVisible((prev) => !prev)}} style={{ background: 'none', border: 'none' }}>
-                {isConfirmPasswordVisible ? <AiOutlineEyeInvisible color="gray" /> : <AiOutlineEye color="gray" />}
-              </button>
-            </InputRightElement>
-          </InputGroup>
-            <FormErrorMessage>
-              {errors.confirmPassword?.message}
-            </FormErrorMessage>
-          </FormControl>
 
-          <HStack
-            pt="10px"
-          >
-              <Button
-                type="submit"
-                size={"lg"}
-                borderRadius={"20px"}
-                sx={{ width: "100%" }}
-              >
-                <ChakraLink as={Link} to="/login">
-                  Cancel
-                </ChakraLink>
-              </Button>
+            {/* Confirm Password Field */}
+            <FormControl isInvalid={!!errors.confirmPassword} className="form-field-container">
+              <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+              <div className="input-outer">
+                <div className="input-icon-container">
+                  <Icon as={AiFillLock} boxSize="24px" color="#718096" />
+                </div>
+                <div className="input-text-container">
+                  <Input
+                    id="confirmPassword"
+                    placeholder="Your password"
+                    type={isConfirmPasswordVisible ? "text" : "password"}
+                    variant="unstyled"
+                    className="input-text"
+                    {...register("confirmPassword")}
+                    isRequired
+                    autoComplete="password"
+                  />
+                </div>
+                <div className="input-right-icon-container">
+                  <button
+                    type="button"
+                    onClick={() => setIsConfirmPasswordVisible(prev => !prev)}
+                    className="icon-button"
+                  >
+                    <Icon as={AiFillEye} boxSize="24px" color="#718096" />
+                  </button>
+                </div>
+              </div>
+              <FormErrorMessage className="form-error">
+                {errors.confirmPassword?.message}
+              </FormErrorMessage>
+            </FormControl>
+          </div>
 
-            <Button
-              type="submit"
-              size={"lg"}
-              sx={{ width: "100%" }}
-              borderRadius={"20px"}
-              bg={"#4E4AE7"}
-              textColor={"white"}
-              isDisabled={Object.keys(errors).length > 0}
-            >
-              Enter
+          {/* Button Group */}
+          <HStack className="button-group">
+            <Button type="button" className="cancel-button">
+              <ChakraLink as={Link} to="/login">Back to Login</ChakraLink>
+            </Button>
+            <Button type="submit" className="submit-button" isDisabled={Object.keys(errors).length > 0}>
+              Create Account
             </Button>
           </HStack>
         </Stack>
