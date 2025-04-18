19908376adca7743d6effa076e5eb5aa5ff68784
Anthony Suh <anthods1@uci.edu>
2025-04-18 13:56:17 -0700

    changed format of program and date

--- client/src/components/filters/ProgramFilter.jsx
+++ client/src/components/filters/ProgramFilter.jsx
@@ -1,4 +1,5 @@
-import React from "react"; 
+import React from "react";
+
 import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
 import {
   Box,
@@ -35,31 +36,18 @@ const ProgramSortingModal = ({ onSortChange }) => {
           className="menu-button-icon"
         />
       </MenuButton>
-      <MenuList
-        sx={{
-          display: "flex",
-          width: "176px",
-          padding: "4px",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          gap: "10px",
-          borderRadius: "4px",
-          border: "1px solid var(--Secondary-3, #E2E8F0)",
-          background: "#FFF",
-          boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.05)"
-        }}
-      >
+      <MenuList className="program-sorting-popup">
         <MenuItem
           onClick={() => onSortChange("title", "asc")}
           width="100%"
         >
-          <Box className="icon-filter-style">A ‑ Z</Box>
+          <Box className="icon-style">A ‑ Z</Box>
         </MenuItem>
         <MenuItem
           onClick={() => onSortChange("title", "desc")}
           width="100%"
         >
-          <Box className="icon-filter-style">Z ‑ A</Box>
+          <Box className="icon-style">Z ‑ A</Box>
         </MenuItem>
       </MenuList>
     </Menu>
