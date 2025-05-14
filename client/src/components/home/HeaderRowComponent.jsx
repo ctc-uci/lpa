017f4ac0afc5723e626d1a9c791b5eecd1a6b46b
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2025-05-14 00:04:24 -0700

    fixed image use

--- client/src/components/home/HeaderRowComponent.jsx
+++ client/src/components/home/HeaderRowComponent.jsx
@@ -14,7 +14,7 @@ export const HeaderRowComponent = () => {
           // handle google calendar logic
         }}
       >
-        <Image
+        <image
           src={googleCalendarSvg}
           alt="Google Calendar"
           className="google-calendar-icon"
