107ece7c4c15cfac31868eccbf3d7937ca941346
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2025-05-13 23:59:50 -0700

    remove unuused import

--- client/src/components/home/HeaderRowComponent.jsx
+++ client/src/components/home/HeaderRowComponent.jsx
@@ -1,5 +1,3 @@
-import { Box, Image, Text } from "@chakra-ui/react";
-
 import { useNavigate } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/react";
 import googleCalendarSvg from "../../assets/icons/google-calendar.svg";
