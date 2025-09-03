33a62682f7de8af4deaedc7f685aab215151043a
Nathan Pietrantonio <pietrann@uci.edu>
2025-09-02 18:37:13 -0700

    Fix: Deleting programs from home
    Fixed error preventing programs from being deleted on the home page

--- client/src/components/cancelModal/CancelProgramComponent.jsx
+++ client/src/components/cancelModal/CancelProgramComponent.jsx
@@ -27,6 +27,7 @@ import { CancelArchiveIcon } from "../../assets/CancelArchiveIcon";
 import { useBackendContext } from "../../contexts/hooks/useBackendContext";
 import { getCurrentUser } from "../../utils/auth/firebase";
 import { archiveProgram, deleteProgram } from "../../utils/programModifications";
+import { useNavigate } from "react-router-dom";
 
 export const CancelProgram = ({
   id,
@@ -46,9 +47,12 @@ export const CancelProgram = ({
   const [submissionLoading, setSubmissionLoading] = useState(false);
   const { backend } = useBackendContext();
   const toast = useToast();
+  const navigate = useNavigate();
 
   useEffect(() => {
     // get event description
+    console.log("id: ", id);
+    if (!id) return;
     const fetchData = async () => {
       const request = await backend.get(`events/${id}`);
       setEventDescription(request.data[0].description);
@@ -57,7 +61,11 @@ export const CancelProgram = ({
         console.log("event description: ", request.data[0].description);
       }
     };
-    fetchData();
+    try {
+      fetchData();
+    } catch (error) {
+      console.log("Error fetching event description: ", error);
+    }
   }, [backend, id]);
 
   const handleSelect = useCallback((action, iconSrc) => {
@@ -129,6 +137,7 @@ export const CancelProgram = ({
         duration: 3000,
         isClosable: true,
       });
+      navigate(`/programs`);
     } catch (error) {
       console.error("Failed to delete program:", error);
       toast({
