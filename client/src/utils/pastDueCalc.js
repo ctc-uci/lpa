ceb605f89b7a6f0dd064c79523989a5503911e37
Nathan Pietrantonio <pietrann@uci.edu>
2025-09-28 14:03:30 -0700

    Fix: spacing

--- client/src/utils/pastDueCalc.js
+++ client/src/utils/pastDueCalc.js
@@ -26,29 +26,29 @@ export const getPastDue = async (backend, id) => {
 export const getAllDue = async (backend, id) => {
     let previousInvoices = await backend.get(`/invoices/previousInvoices/${id}`);
     previousInvoices = previousInvoices.data;
-  
+
     const previousTotals = await Promise.all(
-      previousInvoices.map(async (invoice) => {
+        previousInvoices.map(async (invoice) => {
         const total = await backend.get(`/invoices/total/${invoice.id}`);
         return total.data.total;
-      })
+        })
     );
     let previousTotal = previousTotals.reduce((acc, total) => acc + total, 0);
 
     const currentTotal = await backend.get(`/invoices/total/${id}`);
     previousTotal = currentTotal.data.total;
-  
+
     const paidTotals = await Promise.all(
-      previousInvoices.map(async (invoice) => {
+        previousInvoices.map(async (invoice) => {
         const paid = await backend.get(`/invoices/paid/${invoice.id}`);
         return paid.data.total;
-      })
+        })
     );
     let paidTotal = paidTotals.reduce((acc, paid) => acc + paid, 0);
 
     const currentPaid = await backend.get(`/invoices/paid/${id}`);
     paidTotal = currentPaid.data.total;
-  
+
     const pastDue = (previousTotal - paidTotal) > 0 ? (previousTotal - paidTotal) : 0;
     return pastDue;
-  };
+};
